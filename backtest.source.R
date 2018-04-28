57a656d9726a712650837ba5bbe3a5bf2b861aeb
VickieCao <38790347+VickieCao@users.noreply.github.com>
2018-04-29 00:27:20 +0800

    Update backtest.source.R

--- backtest.source.R
+++ backtest.source.R
@@ -101,7 +101,7 @@ backtest <- function(historical.price, days, r, d, coupon, sigma, initial.price,
                             0.033 * exp(-r * max(1-j/252, 0)) + 
                             0.033 * exp(-r * max(0.5-j/252, 0))) * 
                             initial.price 
-                            || price[j, 1] > 2*initial.price) {
+                            || historical.price[j, 1] > 2*initial.price) {
       cash[j,1] <- cash[j,1] + value[j,1]
       value[j:505,1] <- 0
       for (n in (j+1):505) {

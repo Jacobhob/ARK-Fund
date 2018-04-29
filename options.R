5e17e0c515aae3661590cc88ae457de4640829fc
linpipi <32119372+linpipi@users.noreply.github.com>
2018-04-29 14:16:55 +0800

    pppp

--- options.R
+++ options.R
@@ -2,16 +2,16 @@
 
 library(mnormt)
 
-# The function to calculate the price of a european call.
+# The function to calculate the price of a european put.
 euro.put <- function(s0, sigma, r, d, T, K) { 
-  d1 <- (log(s0 / K) + (r - d + (0.5) * sigma^2) * T)/(sigma * sqrt(T))
+  d1 <- (log(s0 / K) + (r - d + (0.5) * sigma^2) * T) / (sigma * sqrt(T))
   d2 <- d1 - sigma * sqrt(T)
   K * exp(-r * T) * pnorm(-d2) - s0 * exp(-d * T) * pnorm(-d1)
 }
 
-# The function to calculate the price of a european put.
+# The function to calculate the price of a european call.
 euro.call <- function(s0, sigma, r, d, T, K) {
-  d1 <- (log(s0 / K)+(r - d + (0.5) * sigma^2) * T) / (sigma * sqrt(T))
+  d1 <- (log(s0 / K) + (r - d + (0.5) * sigma^2) * T) / (sigma * sqrt(T))
   d2 <- d1 - sigma * sqrt(T)
   pnorm(d1) * s0 * exp(-d * T) - pnorm(d2) * K * exp(-r * (T))
 }
@@ -42,6 +42,19 @@ binormsdist <- function(x1, x2, rho) {
   .d1(s, k, v, r, tt, d) - v*tt^(0.5)
 }
 
+#' @param s Price of the asset on which the underlying option is
+#'     written
+#' @param v Volatility of the underlying asset, defined as the
+#'     annualized standard deviation of the continuously-compounded
+#'     return
+#' @param r Annual continuously-compounded risk-free interest rate
+#' @param d Dividend yield of the underlying asset, annualized,
+#'     continuously-compounded
+#' @param kuo strike on the underlying option
+#' @param kco strike on compound option (the price at which you would
+#'     buy or sell the underlying option at time t1)
+#' @param t1 time until exercise for the compound option
+#' @param t2 time until exercise for the underlying option
 put.on.put.formula <- function(s, kuo, kco, v, r, t1, t2, d) {
   a1 <- .d1(s, s, v, r, t1, d)
   a2 <- a1 - v * t1 ^ 0.5
@@ -50,5 +63,5 @@ put.on.put.formula <- function(s, kuo, kco, v, r, t1, t2, d) {
   temp <- (s * exp(-d * t2) * binormsdist(a1, -d1, -(t1 / t2) ^ 0.5)
            - kuo * exp(-r * t2) * binormsdist(a2, -d2, -(t1 / t2) ^ 0.5)
            + kco * exp(-r * t1) * pnorm(a2))
-  return(c(price=temp))
+  return(max(c(price=temp),0))
 }
